41d3b16353fc4e64909a5238a388e23840c4fe81
Reece Carey <reececarey@msn.com>
2020-02-04 00:33:19 +0000

    Parallax map test GLSL is written. Need to create a depth map for the model within the scene to test.

--- sandbox/assets/shaders/parallaxMapping.glsl
+++ sandbox/assets/shaders/parallaxMapping.glsl
@@ -60,7 +60,10 @@ in vec3 TangentLightPosition;
 in vec3 TangentViewPosition;
 in vec3 TangentFragmentPosition;
 
+float heightScale = 0.1;
+
 // uniform sampler2D diffuseMap;
+uniform sampler2D depthMap;
 uniform sampler2D u_texData;
 uniform sampler2D u_texDataTwo;
 // uniform sampler2D normalMap;
@@ -72,8 +75,58 @@ layout(std140) uniform Light
 	vec3 u_lightColour;
 };
 
+vec2 ParallaxMapping(vec2 texCoords, vec3 viewDir)
+{ 
+    // number of depth layers
+    const float minLayers = 8;
+    const float maxLayers = 32;
+    float numLayers = mix(maxLayers, minLayers, abs(dot(vec3(0.0, 0.0, 1.0), viewDir)));  
+    // calculate the size of each layer
+    float layerDepth = 1.0 / numLayers;
+    // depth of current layer
+    float currentLayerDepth = 0.0;
+    // the amount to shift the texture coordinates per layer (from vector P)
+    vec2 P = viewDir.xy / viewDir.z * heightScale; 
+    vec2 deltaTexCoords = P / numLayers;
+  
+    // get initial values
+    vec2  currentTexCoords     = texCoords;
+    float currentDepthMapValue = texture(depthMap, currentTexCoords).r;
+      
+    while(currentLayerDepth < currentDepthMapValue)
+    {
+        // shift texture coordinates along direction of P
+        currentTexCoords -= deltaTexCoords;
+        // get depthmap value at current texture coordinates
+        currentDepthMapValue = texture(depthMap, currentTexCoords).r;  
+        // get depth of next layer
+        currentLayerDepth += layerDepth;  
+    }
+    
+    // get texture coordinates before collision (reverse operations)
+    vec2 prevTexCoords = currentTexCoords + deltaTexCoords;
+
+    // get depth after and before collision for linear interpolation
+    float afterDepth  = currentDepthMapValue - currentLayerDepth;
+    float beforeDepth = texture(depthMap, prevTexCoords).r - currentLayerDepth + layerDepth;
+ 
+    // interpolation of texture coordinates
+    float weight = afterDepth / (afterDepth - beforeDepth);
+    vec2 finalTexCoords = prevTexCoords * weight + currentTexCoords * (1.0 - weight);
+
+    return finalTexCoords;
+}
+
 void main()
 {
+    // offset texture coordinates with Parallax Mapping
+    vec3 viewDir = normalize(TangentViewPosition - TangentFragmentPosition);
+    vec2 texCoords = TexCoords;
+    
+    texCoords = ParallaxMapping(fs_in.TexCoords,  viewDir);       
+    if(texCoords.x > 1.0 || texCoords.y > 1.0 || texCoords.x < 0.0 || texCoords.y < 0.0)
+        discard;
+
     vec3 normal = texture(u_texData, TexCoords).rgb; //get normals from normal map
 	normal = normalize(normal * 2.0f - 1.0f); //transform normals to be in range <-1; 1> (normal in tangent space)
 
