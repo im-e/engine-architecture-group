0d398754ac54e5b21aa4858a59a51781012271ac
Reece Carey <reececarey@msn.com>
2020-02-04 12:58:39 +0000

    Parallax map changes / added height map

--- sandbox/assets/shaders/parallaxMapping.glsl
+++ sandbox/assets/shaders/parallaxMapping.glsl
@@ -77,19 +77,15 @@ layout(std140) uniform Light
 
 vec2 ParallaxMapping(vec2 texCoords, vec3 viewDir)
 { 
-    // number of depth layers
     const float minLayers = 8;
     const float maxLayers = 32;
     float numLayers = mix(maxLayers, minLayers, abs(dot(vec3(0.0, 0.0, 1.0), viewDir)));  
-    // calculate the size of each layer
     float layerDepth = 1.0 / numLayers;
-    // depth of current layer
     float currentLayerDepth = 0.0;
-    // the amount to shift the texture coordinates per layer (from vector P)
+
     vec2 P = viewDir.xy / viewDir.z * heightScale; 
     vec2 deltaTexCoords = P / numLayers;
   
-    // get initial values
     vec2  currentTexCoords     = texCoords;
     float currentDepthMapValue = texture(depthMap, currentTexCoords).r;
       
@@ -98,19 +94,16 @@ vec2 ParallaxMapping(vec2 texCoords, vec3 viewDir)
         // shift texture coordinates along direction of P
         currentTexCoords -= deltaTexCoords;
         // get depthmap value at current texture coordinates
-        currentDepthMapValue = texture(depthMap, currentTexCoords).r;  
+        currentDepthMapValue = texture(u_texData, currentTexCoords).r;  
         // get depth of next layer
         currentLayerDepth += layerDepth;  
     }
     
-    // get texture coordinates before collision (reverse operations)
     vec2 prevTexCoords = currentTexCoords + deltaTexCoords;
 
-    // get depth after and before collision for linear interpolation
     float afterDepth  = currentDepthMapValue - currentLayerDepth;
-    float beforeDepth = texture(depthMap, prevTexCoords).r - currentLayerDepth + layerDepth;
- 
-    // interpolation of texture coordinates
+    float beforeDepth = texture(u_texData, prevTexCoords).r - currentLayerDepth + layerDepth;
+
     float weight = afterDepth / (afterDepth - beforeDepth);
     vec2 finalTexCoords = prevTexCoords * weight + currentTexCoords * (1.0 - weight);
 
@@ -123,7 +116,7 @@ void main()
     vec3 viewDir = normalize(TangentViewPosition - TangentFragmentPosition);
     vec2 texCoords = TexCoords;
     
-    texCoords = ParallaxMapping(fs_in.TexCoords,  viewDir);       
+    texCoords = ParallaxMapping(TexCoords,  viewDir);       
     if(texCoords.x > 1.0 || texCoords.y > 1.0 || texCoords.x < 0.0 || texCoords.y < 0.0)
         discard;
 
@@ -139,7 +132,7 @@ void main()
     float diff = max(dot(lightDir, normal), 0.0);
     vec3 diffuse = diff * color;
     // specular
-    vec3 viewDir = normalize(TangentViewPosition - TangentFragmentPosition);
+    // vec3 viewDir = normalize(TangentViewPosition - TangentFragmentPosition);
     vec3 reflectDir = reflect(-lightDir, normal);
     vec3 halfwayDir = normalize(lightDir + viewDir);  
     float spec = pow(max(dot(normal, halfwayDir), 0.0), 32.0);
