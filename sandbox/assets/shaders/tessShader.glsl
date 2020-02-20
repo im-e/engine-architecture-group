8f3aaacfd05dbd1d35abfc4b84ec72190cf904a1
Charlie Jenkinson <charliejenko1@outlook.com>
2020-02-20 14:37:38 +0000

    Rearranged tessellation shader so it follows the shader pipeline. (Vertex, Tess, Geo, Frag).

--- sandbox/assets/shaders/tessShader.glsl
+++ sandbox/assets/shaders/tessShader.glsl
@@ -19,101 +19,6 @@ void main()
 		gl_Position = u_model *vec4(aPos, 1.0); 
 }
 				
-#region Fragment
-
-#version 330 core
-out vec4 FragColor;
-
-
-in vec3 gNormals ;
-in vec3 gWorldPos_FS_in ;
-in vec2 gTexCoord;
-
-//struct Material {
-//    vec3 ambient;
-//    vec3 diffuse;
-//    vec3 specular;    
-//    float shininess;
-//};                                                                        
-
-
-layout(std140) uniform TessLightUBO
-{
-	vec3 u_lightPos;
-	vec3 u_ambient;
-	vec3 u_diffuse;
-	vec3 u_specular;
-	vec3 u_viewPos; 
-	vec3 u_lightColour;
-};
-
-//uniform sampler2D texture1;
-//uniform DirLight dirLight;
-//uniform Material mat ;
-//uniform vec3 viewPos ;
-
-uniform sampler2D u_texData;
-
-void main()
-{   
-    
-  
-	float ambientStrength = 0.4;
-	vec3 ambient = ambientStrength * u_lightColour;
-	
-	vec3 norm = normalize(gNormals);
-	vec3 lightDir = normalize(u_lightPos - gWorldPos_FS_in);
-	float diff = max(dot(norm, lightDir), 0.0);
-	vec3 diffuse = diff * u_lightColour;
-	
-	float specularStrength = 0.8;
-	vec3 viewDir = normalize(u_viewPos - gWorldPos_FS_in);
-	vec3 reflectDir = reflect(-lightDir, norm);  
-	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 64);
-	vec3 specular = specularStrength * spec * u_lightColour;  
-	
-	FragColor = vec4((ambient + diffuse + specular), 1.0) * texture(u_texData, gTexCoord);
-
-	
-}
-
-#region Geometry
-
-#version 330 core
-layout(triangles) in ;
-layout(triangle_strip, max_vertices = 3) out ;
-vec3 getNormal() ;
-
-in vec3 fragPos[] ;
-in vec3 normals[] ;
-in vec2 teTexCoord[];
-
-out vec3 gNormals ;
-out vec3 gWorldPos_FS_in ;
-out vec2 gTexCoord;
-
-void main()
-{
-  
-   for(int i = 0 ; i < 3; i++)
-   {
-      gl_Position = gl_in[i].gl_Position ;
-      gWorldPos_FS_in = fragPos[i] ;
-      gNormals = getNormal() ; 
-	  gTexCoord = teTexCoord[i];
-      EmitVertex() ;
-  }
-     EndPrimitive() ;
-
-}
-
-
-vec3 getNormal()
-{
-    vec3 a = vec3(gl_in[1].gl_Position) - vec3(gl_in[0].gl_Position);
-    vec3 b = vec3(gl_in[2].gl_Position) - vec3(gl_in[0].gl_Position);
-    return normalize(cross(a, b));
-}
 
 #region Tessellation Control
 
@@ -245,4 +150,91 @@ vec2 interpolate2D(vec2 v0, vec2 v1, vec2 v2)
 vec3 interpolate3D(vec3 v0, vec3 v1, vec3 v2)
 {
    	return vec3(gl_TessCoord.x) * v0 + vec3(gl_TessCoord.y) * v1 + vec3(gl_TessCoord.z) * v2;
+}
+
+#region Geometry
+
+#version 330 core
+layout(triangles) in ;
+layout(triangle_strip, max_vertices = 3) out ;
+vec3 getNormal() ;
+
+in vec3 fragPos[] ;
+in vec3 normals[] ;
+in vec2 teTexCoord[];
+
+out vec3 gNormals ;
+out vec3 gWorldPos_FS_in ;
+out vec2 gTexCoord;
+
+void main()
+{
+  
+   for(int i = 0 ; i < 3; i++)
+   {
+      gl_Position = gl_in[i].gl_Position ;
+      gWorldPos_FS_in = fragPos[i] ;
+      gNormals = getNormal() ; 
+	  gTexCoord = teTexCoord[i];
+      EmitVertex() ;
+  }
+     EndPrimitive() ;
+
+}
+
+
+vec3 getNormal()
+{
+    vec3 a = vec3(gl_in[1].gl_Position) - vec3(gl_in[0].gl_Position);
+    vec3 b = vec3(gl_in[2].gl_Position) - vec3(gl_in[0].gl_Position);
+    return normalize(cross(a, b));
+}
+
+#region Fragment
+
+#version 330 core
+out vec4 FragColor;
+
+in vec3 gNormals ;
+in vec3 gWorldPos_FS_in ;
+in vec2 gTexCoord;
+                                                                       
+layout(std140) uniform TessLightUBO
+{
+	vec3 u_lightPos;
+	vec3 u_ambient;
+	vec3 u_diffuse;
+	vec3 u_specular;
+	vec3 u_viewPos; 
+	vec3 u_lightColour;
+};
+
+//uniform sampler2D texture1;
+//uniform DirLight dirLight;
+//uniform Material mat ;
+//uniform vec3 viewPos ;
+
+uniform sampler2D u_texData;
+
+void main()
+{   
+    
+  
+	float ambientStrength = 0.4;
+	vec3 ambient = ambientStrength * u_lightColour;
+	
+	vec3 norm = normalize(gNormals);
+	vec3 lightDir = normalize(u_lightPos - gWorldPos_FS_in);
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 diffuse = diff * u_lightColour;
+	
+	float specularStrength = 0.8;
+	vec3 viewDir = normalize(u_viewPos - gWorldPos_FS_in);
+	vec3 reflectDir = reflect(-lightDir, norm);  
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 64);
+	vec3 specular = specularStrength * spec * u_lightColour;  
+	
+	FragColor = vec4((ambient + diffuse + specular), 1.0) * texture(u_texData, gTexCoord);
+
+	
 }
