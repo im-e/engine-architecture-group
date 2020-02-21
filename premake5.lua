e7a1c918990280d6582b469333580938e5a8978d
jankalinowski233 <jan.kalinowski1998@gmail.com>
2020-02-21 00:22:37 +0000

    Added lua submodule (hopefully correctly this time)

--- premake5.lua
+++ premake5.lua
@@ -49,7 +49,7 @@ project "Engine"
 		"vendor/assimp/include",
 		"vendor/reactphysics3d/src",
 		"vendor/ImGui",
-		"vendor/luaAPI",
+		"vendor/lua",
 		"vendor/LuaBridge"
 	}
 	
@@ -325,4 +325,4 @@ group "Vendor"
 	include "vendor/assimp"
 	include "vendor/reactphysics3d"
 	include "vendor/ImGui"
-	include "vendor/luaAPI"
+	include "vendor/lua"
