9457d9f3c7d1df2363306314ca5f4e9d9fbab375
Charlie Jenkinson <charliejenko1@outlook.com>
2020-03-17 18:02:40 +0000

    Added normal map shader with tessellation. Fixed lighting issue on spaceship

---
+++ sandbox/assets/shaders/normalMapping.glsl
@@ -0,0 +1,242 @@
+#region Vertex
+
+#version 440 core
+			
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
+layout (location = 3) in vec3 aTangentVector;
+
+uniform mat4 u_model;
+
+out vec3 FragPos;
+out vec2 TexCoords;
+out vec3 fragNormal;
+out vec3 TangentLightPosition;
+out vec3 TangentViewPosition;
+out vec3 TangentFragmentPosition;
+
+layout(std140) uniform Light
+{
+	vec3 u_lightPos; 
+	vec3 u_viewPos; 
+	vec3 u_lightColour;
+};
+
+void main()
+{
+    FragPos = vec3(u_model * vec4(aPos, 1.0));
+    TexCoords = aTexCoords;
+
+	mat3 normalMatrix = transpose(inverse(mat3(u_model)));
+	vec3 tangent = normalize(normalMatrix*aTangentVector);
+	vec3 normal = normalize(normalMatrix*aNormal);
+	tangent = normalize(tangent - dot(tangent, normal) * normal);
+	vec3 bitangent = cross(tangent, normal);
+
+	mat3 TBNMatrix = transpose(mat3(tangent, bitangent, normal));
+
+	TangentLightPosition = TBNMatrix * u_lightPos;
+	TangentViewPosition = TBNMatrix * u_viewPos;
+	TangentFragmentPosition = TBNMatrix * FragPos;
+    
+    gl_Position =  u_model * vec4(aPos, 1.0);
+}
+
+#region Tessellation Control
+
+#version 450 core
+layout (vertices =3) out;
+
+in vec3 FragPos[];
+in vec3 fragNormal[];
+in vec3 TangentLightPosition[];
+in vec3 TangentViewPosition[];
+in vec3 TangentFragmentPosition[];
+in vec2 TexCoords[];
+
+out vec3 fragPosTC[];
+out vec3 normalTC[];
+out vec3 TangentLightPositionTC[];
+out vec3 TangentViewPositionTC[];
+out vec3 TangentFragmentPositionTC[];
+out vec2 texCoordTC[];
+
+layout(std140) uniform CameraPosition
+{
+	vec3 u_camPos;
+};
+		
+float GetTessLevel(float Distance0, float Distance1)
+{
+	float avgDist = (Distance0 + Distance1) / 2.0f;
+	float tess = 0.0f;
+
+
+	float exponent = -0.105f * avgDist;
+	tess = exp(exponent) * 15.0f;
+	if(tess < 1.0f)
+	{
+		return 1.0f;
+	}
+	tess = 5.0f;
+	return tess;
+}
+
+void main()
+{
+   float eyeToVertexDist1 = distance(u_camPos, fragmentPos[1]);
+   float eyeToVertexDist2 = distance(u_camPos, fragmentPos[2]);
+   if (gl_InvocationID==0)
+   {
+		  fragPosTC[gl_InvocationID]  = fragmentPos[gl_InvocationID];
+
+		   // Calculate the tessellation levels
+          gl_TessLevelOuter[0] = GetTessLevel(eyeToVertexDist1, eyeToVertexDist2); 
+          gl_TessLevelOuter[1] = gl_TessLevelOuter[0]; 
+          gl_TessLevelOuter[2] = gl_TessLevelOuter[0];
+          gl_TessLevelInner[0] = gl_TessLevelOuter[0]; 
+
+   }
+
+   // pass through position and normal information
+   fragPosTC[gl_InvocationID]  = FragPos[gl_InvocationID];
+   normalTC[gl_InvocationID] = fragNormal[gl_InvocationID];
+   texCoordTC[gl_InvocationID] = TexCoords[gl_InvocationID];
+   TangentLightPositionTC[gl_InvocationID] = TangentLightPosition[gl_InvocationID];
+   TangentViewPositionTC[gl_InvocationID] = TangentViewPosition[gl_InvocationID];
+   TangentFragmentPositionTC[gl_InvocationID] = TangentFragmentPosition[gl_InvocationID];
+}
+
+#region Tessellation Eval
+
+#version 450 core
+
+layout(triangles, fractional_even_spacing, ccw) in;
+
+vec2 interpolate2D(vec2 v0, vec2 v1, vec2 v2);
+vec3 interpolate3D(vec3 v0, vec3 v1, vec3 v2);
+
+in vec3 fragPosTC[];
+in vec3 normalTC[];
+in vec3 TangentLightPositionTC[];
+in vec3 TangentViewPositionTC[];
+in vec3 TangentFragmentPositionTC[];
+in vec2 texCoordTC[];
+
+out vec3 fragPosTE;
+out vec3 normalTE;
+out vec3 TangentLightPositionTE;
+out vec3 TangentViewPositionTE;
+out vec3 TangentFragmentPositionTE;
+out vec2 texCoordTE;
+
+layout(std140) uniform Matrices
+{
+	mat4 u_VP;
+};
+
+void main()
+{
+	fragPosTE = interpolate3D(fragPosTC[0], fragPosTC[1], fragPosTC[2]);
+	normalTE = interpolate3D(normalTC[0], normalTC[1], normalTC[2]);
+	texCoordTE = interpolate2D(texCoordTC[0], texCoordTC[1], texCoordTC[2]);
+	TangentLightPositionTE = interpolate3D(TangentLightPositionTC[0], TangentLightPositionTC[1], TangentLightPositionTC[2]);
+	TangentViewPositionTE = interpolate3D(TangentViewPositionTC[0], TangentViewPositionTC[1], TangentViewPositionTC[2]);
+	TangentFragmentPositionTE = interpolate3D(TangentFragmentPositionTC[0], TangentFragmentPositionTC[1], TangentFragmentPositionTC[2]);
+	gl_Position = u_VP * vec4(fragPosTE, 1.0);
+}
+
+//basic linear interpolation
+vec2 interpolate2D(vec2 v0, vec2 v1, vec2 v2)
+{
+   	return vec2(gl_TessCoord.x) * v0 + vec2(gl_TessCoord.y) * v1 + vec2(gl_TessCoord.z) * v2;
+}
+
+vec3 interpolate3D(vec3 v0, vec3 v1, vec3 v2)
+{
+   	return vec3(gl_TessCoord.x) * v0 + vec3(gl_TessCoord.y) * v1 + vec3(gl_TessCoord.z) * v2;
+}
+
+#region Geometry
+
+#version 330 core
+layout(triangles) in;
+layout(triangle_strip, max_vertices = 3) out;
+vec3 getNormal();
+
+in vec3 fragPosTE[];
+in vec3 normalTE[];
+in vec3 TangentLightPositionTE[];
+in vec3 TangentViewPositionTE[];
+in vec3 TangentFragmentPositionTE[];
+in vec2 texCoordTE[];
+
+out vec3 fragPosG;
+out vec3 normalG;
+out vec3 TangentLightPositionG;
+out vec3 TangentViewPositionG;
+out vec3 TangentFragmentPositionG;
+out vec2 texCoordG;
+
+void main()
+{
+	for(int i = 0 ; i < 3; i++)
+   {
+      gl_Position = gl_in[i].gl_Position ;
+      fragPosG = fragPosTE[i] ;
+      normalG = getNormal() ; 
+	  texCoordG = texCoordTE[i];
+	  TangentLightPositionG = TangentLightPositionTE[i];
+	  TangentViewPositionG = TangentViewPositionTE[i];
+	  TangentFragmentPositionG = TangentFragmentPositionTE[i];
+      EmitVertex() ;
+  }
+     EndPrimitive() ;
+}
+
+vec3 getNormal()
+{
+    vec3 a = vec3(gl_in[1].gl_Position) - vec3(gl_in[0].gl_Position);
+    vec3 b = vec3(gl_in[2].gl_Position) - vec3(gl_in[0].gl_Position);
+    return normalize(cross(a, b));
+}
+
+
+#region Fragment
+
+#version 440 core
+			
+layout(location = 0) out vec4 colour;
+
+in vec3 fragPosG;
+in vec3 TangentLightPositionG;
+in vec3 TangentViewPositionG;
+in vec3 TangentFragmentPositionG;
+in vec2 texCoordG;
+
+uniform sampler2D u_texData;
+uniform sampler2D u_normalTexData;
+
+void main()
+{
+    vec3 normal = texture(u_normalTexData, texCoordG).rgb; //get normals from normal map
+	normal = normalize(normal * 2.0f - 1.0f); //transform normals to be in range <-1; 1> (normal in tangent space)
+
+    // get diffuse color
+    vec3 color = texture(u_texData, texCoordG).rgb;
+    // ambient
+    vec3 ambient = 0.1 * color;
+    // diffuse
+    vec3 lightDir = normalize(TangentLightPositionG - TangentFragmentPositionG);
+    float diff = max(dot(lightDir, normal), 0.0);
+    vec3 diffuse = diff * color;
+    // specular
+    vec3 viewDir = normalize(TangentViewPositionG - TangentFragmentPositionG);
+    vec3 reflectDir = reflect(-lightDir, normal);
+    vec3 halfwayDir = normalize(lightDir + viewDir);  
+    float spec = pow(max(dot(normal, halfwayDir), 0.0), 32.0);
+
+    vec3 specular = vec3(0.2) * spec;
+    colour = vec4(ambient + diffuse + specular, 1.0);
+} 
--- sandbox/assets/shaders/tessShader.glsl
+++ sandbox/assets/shaders/tessShader.glsl
@@ -1,42 +1,37 @@
 #region Vertex
 
-#version 450 core
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aNormals;
-layout (location = 2) in vec2 a_texCoord;
+#version 440 core
+			
+layout(location = 0) in vec3 a_vertexPosition;
+layout(location = 1) in vec3 a_vertexNormal;
+layout(location = 2) in vec2 a_texCoord;
+
+out vec3 fragmentPos;
+out vec3 normal;
+out vec2 texCoord;
 
 uniform mat4 u_model;
 
-out vec3 posVS; 
-out vec3 normVS;
-out vec2 texCoord;
-
 void main()
 {
-		texCoord = vec2(a_texCoord.x, a_texCoord.y);
-        normVS = mat3(transpose(inverse(u_model))) * aNormals;
-		posVS = (u_model * vec4(aPos, 1.0)).xyz; 
-		gl_Position = u_model *vec4(aPos, 1.0); 
+	fragmentPos = (u_model * vec4(a_vertexPosition, 1.0)).xyz;
+	normal = mat3(transpose(inverse(u_model))) * a_vertexNormal;
+	texCoord = vec2(a_texCoord.x, a_texCoord.y);
+	gl_Position =  u_model *vec4(a_vertexPosition, 1.0);
 }
-				
 
 #region Tessellation Control
 
 #version 450 core
-layout (vertices =3) out;
+layout (vertices = 3) out;
 
-
-// vectors stored as arrays - each patch has three vertices, each with an xyz pos and xyz norm value 
-//posVS = position from Vertex Shader, posTC = position from this Tesselation Control shader
-
+in vec3 fragmentPos[];
+in vec3 normal[];
 in vec2 texCoord[];
-in vec3 posVS[] ;
-in vec3 normVS[] ;
 
-out vec3 posTC[] ;
-out vec3 normTC[] ;
-out vec3 fragPosCS[];
-out vec2 tcTexCoord[];
+out vec3 fragPosTC[];
+out vec3 normalTC[];
+out vec2 texCoordTC[];
 
 layout(std140) uniform CameraPosition
 {
@@ -61,16 +56,11 @@ float GetTessLevel(float Distance0, float Distance1)
 
 void main()
 {
-   int tessLevel = 1;
-   //float eyeToVertexDist0 = distance(u_camPos, posVS[0]);
-   float eyeToVertexDist1 = distance(u_camPos, posVS[1]);
-   float eyeToVertexDist2 = distance(u_camPos, posVS[2]);
+   float eyeToVertexDist1 = distance(u_camPos, fragmentPos[1]);
+   float eyeToVertexDist2 = distance(u_camPos, fragmentPos[2]);
    if (gl_InvocationID==0)
    {
-		  fragPosCS[gl_InvocationID] = posVS[gl_InvocationID];
-		 // fragPosCS[0] = posVS[0];
-		  //fragPosCS[1] = posVS[1];
-		 // fragPosCS[2] = posVS[2];
+		  fragPosTC[gl_InvocationID]  = fragmentPos[gl_InvocationID];
 
 		   // Calculate the tessellation levels
           gl_TessLevelOuter[0] = GetTessLevel(eyeToVertexDist1, eyeToVertexDist2); 
@@ -81,12 +71,10 @@ void main()
    }
 
    // pass through position and normal information
-   posTC[gl_InvocationID]  = posVS[gl_InvocationID];
-   normTC[gl_InvocationID] = normVS[gl_InvocationID];
-   tcTexCoord[gl_InvocationID] = texCoord[gl_InvocationID];
-
+   fragPosTC[gl_InvocationID]  = fragmentPos[gl_InvocationID];
+   normalTC[gl_InvocationID] = normal[gl_InvocationID];
+   texCoordTC[gl_InvocationID] = texCoord[gl_InvocationID];
 }
-
 
 #region Tessellation Eval
 
@@ -97,34 +85,27 @@ layout(triangles, fractional_even_spacing, ccw) in;
 vec2 interpolate2D(vec2 v0, vec2 v1, vec2 v2);
 vec3 interpolate3D(vec3 v0, vec3 v1, vec3 v2);
 
-
 layout(std140) uniform Matrices
 {
 	mat4 u_VP;
 };
 
-// read in vector arrays from previous shader
-in vec3 posTC[] ;
-in vec3 normTC[] ;
-in vec2 tcTexCoord[];
-in vec3 fragPosCS[];
+in vec3 fragPosTC[];
+in vec3 normalTC[];
+in vec2 texCoordTC[];
 
-// pass along the interpolated values
-out vec3 normES ;
-out vec3 fragPos;
-out vec2 teTexCoord;
+out vec3 fragPosTE;
+out vec3 normalTE;
+out vec2 texCoordTE;
 
 void main()
 {
+	fragPosTE = interpolate3D(fragPosTC[0], fragPosTC[1], fragPosTC[2]);
+	normalTE = interpolate3D(normalTC[0], normalTC[1], normalTC[2]);
+	texCoordTE = interpolate2D(texCoordTC[0], texCoordTC[1], texCoordTC[2]);
 
-   teTexCoord = interpolate2D(tcTexCoord[0], tcTexCoord[1], tcTexCoord[2]);
-   normES = interpolate3D(normTC[0], normTC[1], normTC[2]) ;
-   vec3 posES = interpolate3D(posTC[0], posTC[1], posTC[2]) ;
-   fragPos = interpolate3D(fragPosCS[0], fragPosCS[1], fragPosCS[2]);
-
-   gl_Position = u_VP * vec4(posES, 1.0);
-} 
-
+	gl_Position = u_VP * vec4(fragPosTE, 1.0);
+}
 
 //basic linear interpolation
 vec2 interpolate2D(vec2 v0, vec2 v1, vec2 v2)
@@ -137,36 +118,33 @@ vec3 interpolate3D(vec3 v0, vec3 v1, vec3 v2)
    	return vec3(gl_TessCoord.x) * v0 + vec3(gl_TessCoord.y) * v1 + vec3(gl_TessCoord.z) * v2;
 }
 
+
 #region Geometry
 
 #version 330 core
-layout(triangles) in ;
-layout(triangle_strip, max_vertices = 3) out ;
-vec3 getNormal() ;
+layout(triangles) in;
+layout(triangle_strip, max_vertices = 3) out;
+vec3 getNormal();
 
-in vec3 fragPos[] ;
-//in vec3 normals[] ;
-in vec2 teTexCoord[];
+in vec3 fragPosTE[];
+in vec2 texCoordTE[];
 
-out vec3 gNormals ;
-out vec3 gWorldPos_FS_in ;
-out vec2 gTexCoord;
+out vec3 fragPosG;
+out vec3 normalG;
+out vec2 texCoordG;
 
 void main()
 {
-  
-   for(int i = 0 ; i < 3; i++)
+	for(int i = 0 ; i < 3; i++)
    {
       gl_Position = gl_in[i].gl_Position ;
-      gWorldPos_FS_in = fragPos[i] ;
-      gNormals = getNormal() ; 
-	  gTexCoord = teTexCoord[i];
+      fragPosG = fragPosTE[i] ;
+      normalG = getNormal() ; 
+	  texCoordG = texCoordTE[i];
       EmitVertex() ;
   }
      EndPrimitive() ;
-
 }
-
 
 vec3 getNormal()
 {
@@ -175,21 +153,20 @@ vec3 getNormal()
     return normalize(cross(a, b));
 }
 
+
 #region Fragment
 
 #version 440 core
-out vec4 FragColor;
+			
+layout(location = 0) out vec4 colour;
 
-in vec3 gNormals ;
-in vec3 gWorldPos_FS_in ;
-in vec2 gTexCoord;
-                                                                       
-layout(std140) uniform TessLightUBO
+in vec3 normalG;
+in vec3 fragPosG;
+in vec2 texCoordG;
+
+layout(std140) uniform Light
 {
-	vec3 u_lightPos;
-	vec3 u_ambient;
-	vec3 u_diffuse;
-	vec3 u_specular;
+	vec3 u_lightPos; 
 	vec3 u_viewPos; 
 	vec3 u_lightColour;
 };
@@ -197,22 +174,20 @@ layout(std140) uniform TessLightUBO
 uniform sampler2D u_texData;
 
 void main()
-{   
-	
+{
 	float ambientStrength = 0.4;
 	vec3 ambient = ambientStrength * u_lightColour;
 	
-	vec3 norm = normalize(gNormals);
-	vec3 lightDir = normalize(u_lightPos - gWorldPos_FS_in);
-	float diff = max(dot(lightDir, norm), 0.0);
+	vec3 norm = normalize(normalG);
+	vec3 lightDir = normalize(u_lightPos - fragPosG);
+	float diff = max(dot(norm, lightDir), 0.0);
 	vec3 diffuse = diff * u_lightColour;
 	
 	float specularStrength = 0.8;
-	vec3 viewDir = normalize(u_viewPos - gWorldPos_FS_in);
-	vec3 reflectDir = reflect(-lightDir, norm);
-	vec3 halfwayDir = normalize(lightDir + viewDir);
-	float spec = pow(max(dot(norm, halfwayDir), 0.0), 64.0);
+	vec3 viewDir = normalize(u_viewPos - fragPosG);
+	vec3 reflectDir = reflect(-lightDir, norm);  
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 64);
 	vec3 specular = specularStrength * spec * u_lightColour;  
 	
-	FragColor = vec4((ambient + diffuse + specular), 1.0) * texture(u_texData, gTexCoord);
+	colour = vec4((ambient + diffuse + specular), 1.0) * texture(u_texData, texCoordG);
 }
