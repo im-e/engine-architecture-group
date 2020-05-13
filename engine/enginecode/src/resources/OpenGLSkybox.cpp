4becc6b49ba935be9f7861fc734d0ef19d7feed9
Charlie Jenkinson <charliejenko1@outlook.com>
2020-05-13 11:29:02 +0100

    fixed file issue with openglskybox

---
+++ engine/enginecode/src/resources/OpenGLSkybox.cpp
@@ -0,0 +1,32 @@
+#include "engine_pch.h"
+#include "resources/OpenGLSkybox.h"
+
+OpenGLSkybox::OpenGLSkybox()
+{
+	m_paths.push_back("Assets/Textures/Skybox/Space_Right.png");
+	m_paths.push_back("Assets/Textures/Skybox/Space_left.png");
+	m_paths.push_back("Assets/Textures/Skybox/Space_top.png");
+	m_paths.push_back("Assets/Textures/Skybox/Space_bot.png");
+	m_paths.push_back("Assets/Textures/Skybox/Space_front.png");
+	m_paths.push_back("Assets/Textures/Skybox/Space_back.png");
+}
+
+void OpenGLSkybox::draw()
+{
+	glDepthFunc(GL_LEQUAL); //change depth function so depth test passes when values are equal to depth buffer's content
+	glBindVertexArray(vao);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, texID);
+
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+	glBindVertexArray(0);
+	glDepthFunc(GL_LESS); //change depth check back to normal
+}
+
+void OpenGLSkybox::stop()
+{
+	glDeleteBuffers(1, &vao);
+	glDeleteBuffers(1, &vbo);
+}
